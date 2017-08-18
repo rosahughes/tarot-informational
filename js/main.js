c4c3f8f88a8e6537a0c5f4fe992f8afa3aff1a3b
hughesr <hughesr@seattleu.edu>
2017-08-18 15:42:27 -0400

    update JS 5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // Accordion menu funtion
-let accordion = document.querySelectorAll("accordion");
-let i;
+var accordion = document.querySelectorAll(".accordion");
+var i;
 
 for (i = 0; i < accordion.length; i++) {
     accordion[i].onclick = function(){
-      console.log("Run accordion click function.");
+      console.log("Run accordion click function");
         this.classList.toggle("active");
-        let panel = this.nextElementSibling;
+        var panel = this.nextElementSibling;
         if (panel.style.display === "panel") {
-            panel.style.display = "none"; // *if un-clicked
+            panel.style.display = "none"; // if un-clicked
         } else {
-            panel.style.display = "panel"; // *if clicked
+            panel.style.display = "panel"; // if clicked
         }
     }
 }
